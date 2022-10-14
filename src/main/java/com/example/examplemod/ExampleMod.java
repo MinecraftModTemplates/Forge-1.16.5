0f872aaef2465643728c94e11afff931e9c0fb71
psyGamer <psygamer21@gmail.com>
2022-10-14 22:46:46 +0200

    Initial commit

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,18 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(ExampleMod.MODID)
+public class ExampleMod {
+	
+	public static final String MODID = "examplemod";
+	
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	public ExampleMod() {
+	
+	}
+}
